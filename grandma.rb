a90db6866622809ba9b9b87edd8d3bfe546ecb95
Amin Amos <aminamosahs@gmail.com>
2019-06-13 03:43:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 
 def speak_to_grandma(words)
   if words == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif words == words.upcase
     "NO, NOT SINCE 1938!"
   else
